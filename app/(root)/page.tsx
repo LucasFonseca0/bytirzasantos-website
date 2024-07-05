7bf22241b42ecc823b0f975984479a3b6ae092d2
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-07-05 18:42:15 +0100

    fix: added gold details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <main className="text-gray-900">
       <header className="flex h-60 justify-around items-center">
-        <h1 className="text-3xl font-semibold">bytirzasantos</h1>
+        <h1 className="text-3xl font-semibold text-gold">bytirzasantos</h1>
         <Avatar className="w-32 h-32">
           <AvatarImage src="/images/photo1.jpg" alt="bytirzasantos image" />
           <AvatarFallback>BZ</AvatarFallback>
         </Avatar>
       </header>
       <section className="flex flex-col items-center sm:px-[17%]">
-        <div className="w-full  md:max-w-[750px] h-[50vh] max-h-[500px] md:min-h-[500px] bg-[url('/images/photo2.png')] bg-cover bg-center bg-no-repeat rounded"></div>
+      <div className="w-full md:max-w-[750px] h-[50vh] max-h-[500px] md:min-h-[500px] bg-[url('/images/photo2.png')] bg-cover bg-top bg-no-repeat rounded"></div>
         <div className="flex flex-col items-start md:max-w-[750px] p-[17%] sm:p-0">
           <h2 className="font-bold my-8 text-3xl ">Quem sou eu?</h2>
           <p className="text-xl [&>span]:highlight">
--- app/Components/CarouselSection.tsx
+++ app/Components/CarouselSection.tsx
@@ -11,43 +11,42 @@ import {
 import Autoplay, { AutoplayType } from "embla-carousel-autoplay";
 import Image from 'next/image';
 
-
-interface CarouselItensInterface{
-  title: string
-  description: string
-  imageURL: string
-  LinkURL: string
-  LinkText:string
+interface CarouselItensInterface {
+  title: string;
+  description: string;
+  imageURL: string;
+  LinkURL: string;
+  LinkText: string;
 }
 
-const CaroulselItens:CarouselItensInterface[] = [
+const CaroulselItens: CarouselItensInterface[] = [
   {
     title: "GERENCIAMENTO GOLD",
-    description:"Está sem tempo para cuidar das redes sociais do seu negócio? Então, Deixa comigo",
-    imageURL:"/CarouselImages/photo3.png",
-    LinkURL:"https://docs.google.com/forms/d/e/1FAIpQLSd8iQC6NJsaFrvpV64yrBCfjuqr5l6dvQd9vlC4ErIWGeSw1w/viewform",
-    LinkText:"Formúlario"
+    description: "Está sem tempo para cuidar das redes sociais do seu negócio? Então, Deixa comigo",
+    imageURL: "/CarouselImages/photo3.png",
+    LinkURL: "https://docs.google.com/forms/d/e/1FAIpQLSd8iQC6NJsaFrvpV64yrBCfjuqr5l6dvQd9vlC4ErIWGeSw1w/viewform",
+    LinkText: "Formúlario"
   },
   {
     title: "CG - CONSULTORIA GOLD",
-    description:"Sente que está precisando de dar um UP no seu perfil? a CG pode te ajudar",
-    imageURL:"/CarouselImages/photo4.png",
-    LinkURL:"https://api.whatsapp.com/send/?phone=5573988893577&text=Ol%C3%A1+Tirza%2C+eu+quero+contratar+a+sua+consultoria&type=phone_number&app_absent=0",
-    LinkText:"Entre em contato"
+    description: "Sente que está precisando de dar um UP no seu perfil? a CG pode te ajudar",
+    imageURL: "/CarouselImages/photo4.png",
+    LinkURL: "https://api.whatsapp.com/send/?phone=5573988893577&text=Ol%C3%A1+Tirza%2C+eu+quero+contratar+a+sua+consultoria&type=phone_number&app_absent=0",
+    LinkText: "Entre em contato"
   },
   {
     title: "INSTAGOLD IDV",
-    description:"Invista no seu negócio e tenha um perfil intencional com uma Identidade marcante",
-    imageURL:"/CarouselImages/photo1.png",
-    LinkURL:"https://pay.kiwify.com.br/Icsy5Gi",
-    LinkText:"Eu quero"
+    description: "Invista no seu negócio e tenha um perfil intencional com uma Identidade marcante",
+    imageURL: "/CarouselImages/photo1.png",
+    LinkURL: "https://pay.kiwify.com.br/Icsy5Gi",
+    LinkText: "Eu quero"
   },
   {
     title: "MGS E-BOOK",
-    description:"O meu método para otimizar seu perfil, com estratégias de conteúdos e desafios.",
-    imageURL:"/CarouselImages/photo2.png",
-    LinkURL:"https://metodogoldspark.my.canva.site/",
-    LinkText:"Quero o método"
+    description: "O meu método para otimizar seu perfil, com estratégias de conteúdos e desafios.",
+    imageURL: "/CarouselImages/photo2.png",
+    LinkURL: "https://metodogoldspark.my.canva.site/",
+    LinkText: "Quero o método"
   },
 ]
 
@@ -55,40 +54,37 @@ const CarouselSection = () => {
   const plugin = useRef<AutoplayType | null>(null);
 
   useEffect(() => {
-    plugin.current = Autoplay({ delay: 3000, stopOnInteraction: true,});
+    plugin.current = Autoplay({ delay: 3000, stopOnInteraction: true });
   }, []);
 
   return (
     <section className='flex flex-col items-center mb-40 '>
-      <h2 className='text-lg sm:text-xl font-semibold mt-10 mb-8'>Produtos que você precisa conhecer:</h2>
+      <h2 className='text-lg sm:text-xl font-semibold mt-10 mb-8 text-gold'>Produtos que você precisa conhecer:</h2>
       <Carousel
         plugins={plugin.current ? [plugin.current] : []}
         className="w-[70%] md:w-full md:max-w-xl lg:max-w-3xl "
         onMouseEnter={() => plugin.current && plugin.current.stop()}
         onMouseLeave={() => plugin.current && plugin.current.reset()}
-        
       >
         <CarouselContent>
           {CaroulselItens.map((item, index) => (
-            <CarouselItem key={index} className='basis-2/3 sm:basis-1/2  '>
-              <div className=" bg-primary-foreground rounded">
+            <CarouselItem key={index} className='basis-2/3 sm:basis-1/2 '>
+              <div className=" bg-primary-foreground rounded border-2 border-gold">
                 <Image src={item.imageURL} alt={`imagem do produto ${item.title}`} width={4000} height={4000} className='rounded'></Image>
                 <div className='p-4'>
-                  <h3 className='text-base sm:text-lg font-semibold mb-4'>{item.title}</h3>
+                  <h3 className='text-base sm:text-lg font-semibold mb-4 text-gold'>{item.title}</h3>
                   <p className='mb-10'>{item.description}</p>
-                  <a href={item.LinkURL} className=' underline font-semibold'>{item.LinkText}</a>
+                  <a href={item.LinkURL} className=' underline font-semibold text-gold'>{item.LinkText}</a>
                 </div>
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
         <CarouselPrevious />
-        <CarouselNext/>
+        <CarouselNext />
       </Carousel>
     </section>
   );
 };
-
-
 
 export default CarouselSection;
